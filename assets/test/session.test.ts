7140e1fd618bca434172b6498e4586b3b6fbd407
Tony <anthony.bryan@outlook.com>
2020-04-03 08:43:05 +0100

    test session methods add/delete match and to/from JSON

--- assets/test/session.test.ts
+++ assets/test/session.test.ts
@@ -25,9 +25,10 @@ let testMatches = new List<Match>([
     new Match({ teams: testTeams.getByIndex([0, 1]) }),
     new Match({ teams: testTeams.getByIndex([2, 3]) }),
     new Match({ teams: testTeams.getByIndex([2, 4]) }),
+    new Match({ teams: testTeams.getByIndex([0, 2]) }),
 ]);
 
-let testSession = new Session({players: testPlayers, teams: testTeams, matches: testMatches});
+let testSession = new Session({players: testPlayers, teams: testTeams, matches: <List<Match>>testMatches.getByIndex([0, 1, 2])});
 
 test('getMatchesWithPlayer', () => {
     expect(new List<Match>(testSession.getMatchesWithPlayer(<Player>testPlayers.getByIndex(0)))).toEqual(testMatches.getByIndex([0, 1]));
@@ -37,6 +38,26 @@ test('getMatchesWithTeam', () => {
     expect(new List<Match>(testSession.getMatchesWithTeam(<Team>testTeams.getByIndex(2)))).toEqual(testMatches.getByIndex([1, 2]));
 });
 
+test('deleteMatch should remove only that match', () => {
+    expect(testSession.matches).toEqual(testMatches.getByIndex([0, 1, 2]));
+    testSession.deleteMatch((<Match>testMatches.getByIndex(2)).id);
+    expect(testSession.matches).toEqual(testMatches.getByIndex([0, 1]));
+    testSession.addMatch(<Match>testMatches.getByIndex(2));
+    expect(testSession.matches).toEqual(testMatches.getByIndex([0, 1, 2]));
+});
+
+test('addMatch should add that match', () => {
+    expect(testSession.matches).toEqual(testMatches.getByIndex([0, 1, 2]));
+    testSession.addMatch(<Match>testMatches.getByIndex(3));
+    expect(testSession.matches).toEqual(testMatches.getByIndex([0, 1, 2, 3]));
+    testSession.deleteMatch((<Match>testMatches.getByIndex(3)).id);
+    expect(testSession.matches).toEqual(testMatches.getByIndex([0, 1, 2]));
+});
+
 test('fromObj(toObj) should equal original', () => {
     expect(Session.fromObj(testSession.toObj())).toMatchObject(testSession);
+});
+
+test('fromJSON(toJSON) should equal original', () => {
+    expect(Session.fromJSON(testSession.toJSON())).toMatchObject(testSession);
 });
